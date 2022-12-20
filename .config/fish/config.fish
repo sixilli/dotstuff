c27a47a8be0fd5a3c6702d02978cba9a34536ba9
Alec <alecperro@gmail.com>
2022-12-19 22:17:59 -0600

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
 
     set -U fish_greeting
     fish_add_path -a ~/.local/bin
+    fish_add_path -a /var/lib
 
     alias config="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
 end
